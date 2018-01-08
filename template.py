e8d9158ade81036cb3652c85e4cba1ac4b8a8b1d
苦叶子 <lyy@kuyezideMacBook-Pro.local>
2018-01-08 18:17:50 +0800

    add:新增类定义及其他注释说明

--- template.py
+++ template.py
@@ -3,6 +3,20 @@
 # author 改为你的
 __author__ == "author"
 
-if __name__ == "__main__":
 
+class MyClass:
+    “”“
+        类作用注释说明
+    ”“”
+    def __init__(self):
+        """
+            函数注释说明
+        """
+        pass
+
+        
+if __name__ == "__main__":
+    “”“
+        主函数入口
+    ”“”
     pass
