2ff40aa71d44b2d9d0fb143f482f052285d79a4b
huangpeishan <huangpeishan@bluemoon.com.cn>
2018-01-10 08:42:35 +0800

    删除昨晚备份
    Former-commit-id: 9b26f0814238963782fe664b4b170a683a0032cb
    Former-commit-id: 8a6cedb8d2c1ef6e0a99b959cd98501e46bd794e

--- 第一期/广州-33/calc_33.py
+++
@@ -1,12 +0,0 @@
-#coding=utf-8
-#实现一个四则运算的类，要求实现任意两个数的加减乘除运算
-
-if __name__ == '__main__':
-    a=int(raw_input("请输入一个数字："))
-    b=int(raw_input("请输入一个数字："))
-    print"第一个数字是"%(a+b)
-    #print"第二个数字是"+b
-    pass
-
-class calc:        
-    pass
